259f7e89c32a9b0be7ca8e5323703fa12986a457
monod <shalry0418@gmail.com>
2018-07-18 11:56:20 +0900

    ボール射出システム作成 ホームベースとか簡易的に地形作成～～～

---
+++ Assets/Scripts/BallMove_Basic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove_Basic : MonoBehaviour {
+
+    public float Speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = this.transform.position + (this.transform.forward * Time.deltaTime * Speed);
+	}
+}
---
+++ Assets/Scripts/BallShooter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooter : MonoBehaviour {
+
+    [SerializeField, Tooltip("ボール")]
+    public GameObject[] BallList;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // ボールの射出
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           GameObject go = Instantiate(BallList[0]);
+            go.transform.position = this.transform.position;
+            go.transform.rotation = this.transform.rotation;
+        }
+    }
+}
